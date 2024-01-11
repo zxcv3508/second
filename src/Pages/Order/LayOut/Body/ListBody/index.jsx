52e31c4e188fc3a5dba323e168c75dca113f9765
zxcv3508 <zxcv3508@navre.com>
2024-01-11 18:36:44 +0900

    comment: ListBody 주석 추가

--- src/Pages/Order/LayOut/Body/ListBody/index.jsx
+++ src/Pages/Order/LayOut/Body/ListBody/index.jsx
@@ -7,6 +7,7 @@ import ListBodyElement from "./ListElement";
 
 // 렌더링 최적화를 위해 해당 ListBody에서는 전역 상태 전체를 관리하고
 // 하위 컴포넌트인 ListBodyElement는 memo로 감싸서 props값이 변경될 경우만 렌더
+// useCallback활용해서 props로 넘어갈때 함수가 변경됨을 방지
 const ListBody = ({ itemList }) => {
   const [shoppingCart, setShoppingCart] = useRecoilState(shoppingCartState);
   const setCountAndPrice = useSetRecoilState(countAndPriceState);
